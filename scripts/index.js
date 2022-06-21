de2fb46ed56af406f46986d5064eb46d69f21744
Mikhail Laguzov <m-laguzov@yandex.ru>
2022-06-21 12:37:26 +0300

    Завершение калькулятора

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,37 @@
 const calcInputSum = document.querySelector('.calculator__input-sum');
-const calcNdsInput = document.querySelector('.form-group__input');
-const calcButtonResult = document.querySelector('.form-group__submit');
-const calcForm = document.querySelector('.calculator__form');
+const calcVat = document.querySelector('.form-group__input');
+const template = document.querySelector('#template-result').content;
+const button = document.querySelector('.form-group__submit');
+const form = document.querySelector('.calculator__form');
+const radioType = document.querySelectorAll('input[name="action"]');
 
+// Начисления НДС
+function calcPercent() {
 
-calcButtonResult.addEventListener('click', function() {
-    const calcTemplate = document.querySelector('#template-result').content;
-    const templateClone = calcTemplate.querySelector('.element-result').cloneNode(true);
-    let numberPercent = calcInputSum.value / 100 * calcNdsInput.value;
-    // calcTemplate.querySelector('.element-result__item_type_sum') = 1111;
-    calcForm.append(`Результат с НДС: ${numberPercent}`);
-    console.log(numberPercent);
+    return +calcInputSum.value / 100 * +calcVat.value;
+
+}
+// Вычисление выделение НДС
+function allocateVat() {
+
+    return +calcInputSum.value * +calcVat.value / (+calcVat.value + 100);
+
+}
+
+button.addEventListener('click', function() {
+
+    const templateClone = template.querySelector('.element-result').cloneNode(true);
+
+    if (radioType[0].checked === true) {
+        templateClone.querySelector('.element-result__item_type_sum').textContent = `Cумма без НДС: ${(+calcInputSum.value - +allocateVat().toFixed(2)).toFixed(2)} Р`;
+        templateClone.querySelector('.element-result__item_type_vat').textContent = `НДС ${calcVat.value}%: ${allocateVat().toFixed(2)} Р`;
+        templateClone.querySelector('.element-result__item_type_result-sum').textContent = `Всего: ${+calcInputSum.value} Р`;
+        form.append(templateClone);
+
+    } else if (radioType[1].checked === true) {
+        templateClone.querySelector('.element-result__item_type_sum').textContent = `Cумма без НДС: ${+calcInputSum.value} Р`;
+        templateClone.querySelector('.element-result__item_type_vat').textContent = `НДС: ${+calcVat.value}% ${calcPercent()} Р`;
+        templateClone.querySelector('.element-result__item_type_result-sum').textContent = `Всего: ${+calcInputSum.value + +calcPercent()} Р`;
+        form.append(templateClone);
+    }
 })
